1173b32afcc567a1ba25f95b3d9ca2badf09fffa
Victor Santos Barreto <denvictor.sbarreto@gmail.com>
2023-10-09 12:12:08 -0300

    feat: Funcionalidade de levar para o topo

---
+++ topo.js
@@ -0,0 +1,15 @@
+window.onscroll = function() { scrollFunction() };
+
+function scrollFunction() {
+    var topButton = document.getElementById("topButton");
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        topButton.style.display = "block";
+    } else {
+        topButton.style.display = "none";
+    }
+}
+
+function scrollTopFunction() {
+    document.body.scrollTop = 0; // Para navegadores Safari
+    document.documentElement.scrollTop = 0; // Para outros navegadores
+}
