ec74b8c26393c2ccdfb9a7779e1d73f0e3c67af8
Victor Santos Barreto <denvictor.sbarreto@gmail.com>
2023-10-11 12:52:56 -0300

    refactor: Tela de Login

--- script.js
+++ script.js
@@ -1,19 +1,31 @@
 const cards = document.querySelectorAll(".card-slider");
 const prevButton = document.getElementById("prevButton");
 const nextButton = document.getElementById("nextButton");
-
 let currentIndex = 0;
 
-prevButton.addEventListener("click", () => {
-  cards[currentIndex].classList.remove("active");
-  currentIndex = (currentIndex - 1 + cards.length) % cards.length;
-  cards[currentIndex].classList.add("active");
-});
-
-nextButton.addEventListener("click", () => {
+// Função para avançar para o próximo cartão
+function goToNextCard() {
   cards[currentIndex].classList.remove("active");
   currentIndex = (currentIndex + 1) % cards.length;
   cards[currentIndex].classList.add("active");
+}
+
+// Adicione um temporizador para avançar a cada 5 segundos
+const autoAdvanceInterval = 5000; // 5 segundos
+let autoAdvanceTimer = setInterval(goToNextCard, autoAdvanceInterval);
+
+// Adicione manipuladores de eventos para parar o temporizador quando os botões são clicados
+prevButton.addEventListener("click", () => {
+  clearInterval(autoAdvanceTimer); // Pare o temporizador
+  goToNextCard(); // Avance para o próximo cartão
+  autoAdvanceTimer = setInterval(goToNextCard, autoAdvanceInterval); // Inicie o temporizador novamente
 });
 
+nextButton.addEventListener("click", () => {
+  clearInterval(autoAdvanceTimer); // Pare o temporizador
+  goToNextCard(); // Avance para o próximo cartão
+  autoAdvanceTimer = setInterval(goToNextCard, autoAdvanceInterval); // Inicie o temporizador novamente
+});
+
+// Inicie o primeiro cartão como ativo
 cards[currentIndex].classList.add("active");
